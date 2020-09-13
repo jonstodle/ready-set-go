efa7382eb3450b4fcbd052f6a9321186c0919518
Jon Grythe Stødle <jonstodle@outlook.com>
2020-09-13 19:14:42 +0200

    Greeting web server

--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
-	"flag"
 	"fmt"
+	"net/http"
 	"strings"
 )
 
-func main() {
-	symbol := flag.String("symbol", "!", "The trailing symbol of the greeting")
-	flag.Parse()
+func hello(w http.ResponseWriter, r *http.Request) {
+	var segments = strings.Split(r.URL.Path, "/")
 
-	fmt.Printf("Hello %v%v", strings.Join(flag.Args(), ", "), *symbol)
+	fmt.Fprintf(w, "Hello %v!", segments[len(segments) - 1])
+}
+
+func main() {
+	http.HandleFunc("/hello/", hello)
+
+	println("Listening on port 3333")
+	http.ListenAndServe(":3333", nil)
 }
